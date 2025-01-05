37739dcc0df838cb634081a3f849aa10e9d41949
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-01-05 22:46:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function validEmail(str) {
   //your JS code here.
+return /^[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.[a-zA-Z]+)*(\.[a-zA-Z]{2,3})$/.test(str);
 }
 
 // Do not change the code below.
